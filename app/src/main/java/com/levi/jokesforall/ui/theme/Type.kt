ea26d3d36a595b44595f789ab0558e43fd7b16ff
leviNunez <levi.jupiter@gmail.com>
2025-08-20 19:04:07 -0400

    merge from develop

--- app/src/main/java/com/levi/jokesforall/ui/theme/Type.kt
+++ app/src/main/java/com/levi/jokesforall/ui/theme/Type.kt
@@ -2,14 +2,22 @@ package com.levi.jokesforall.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.levi.jokesforall.R
+
+val adobeDia = FontFamily(
+    Font(R.font.adobe_dia, FontWeight.Normal),
+    Font(R.font.adobe_dia, FontWeight.Medium),
+    Font(R.font.adobe_dia, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = adobeDia,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
