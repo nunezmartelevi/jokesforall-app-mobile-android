f1427ad14a594575a50afc4bb152f6b38a395376
leviNunez <levi.jupiter@gmail.com>
2025-09-07 15:22:33 -0400

    Update colors and strings

--- app/src/main/java/com/levi/jokesforall/ui/theme/Color.kt
+++ app/src/main/java/com/levi/jokesforall/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package com.levi.jokesforall.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Wood = Color(0xFF5D3118)
-val LightBlack = Color(0xFF2e312b)
-val DarkGrey = Color(0xFF1d211a)
+val LightBlack = Color(0xFF2E312B)
+val DarkGrey = Color(0xFF1D211A)
 val BrightGreenTerminal = Color(0xFF8EBF73)
 val DarkGreenTerminal = Color(0xFF7AA860)
